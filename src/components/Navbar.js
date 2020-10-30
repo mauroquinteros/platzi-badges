0957c359476e5c9bef3a18443d54b05f5508a9f9
mauroquinteros <quinterosmauro0599@gmail.com>
2020-10-30 18:31:34 -0500

    Add Navbar component - V0.0.2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,32 @@
 import React from "react";
+import {Link} from 'react-router-dom'
+
+// Assets
+import logoBadge from '../assets/images/logo-badge.svg'
+import '../assets/sass/components/navbar.scss'
 
 const Navbar = () => {
   return (
-    <nav>
-      <h1>Navbar</h1>
+    <nav className="Navbar">
+      <section className="Navbar__container">
+        <Link className="Navbar__link Navbar__link-logo" to="/">
+          <figure className="Navbar__logo">
+            <img src={logoBadge} alt="platzi logo badge" loading="lazy"/>
+          </figure>
+          <span className="fw-light fs-medium">
+            Platzi
+          </span>
+          <span className="fw-bold fs-medium">badges</span>
+        </Link>
+        <div className="Navbar__link-container">
+          <Link className="Navbar__link p-1 fw-light" to="/badges">
+            Lista de Badges
+          </Link>
+          <Link className="Navbar__link p-1 fw-light" to="/badges/new">
+            Agregar participante
+          </Link>
+        </div>
+      </section>
     </nav>
   );
 };
