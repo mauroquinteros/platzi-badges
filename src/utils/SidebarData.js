a86d9d217abf51d88f802ba907a87f370a15ec8e
mauroquinteros <quinterosmauro0599@gmail.com>
2020-11-18 22:38:37 -0500

    Fix Navbar styles - v0.4.5

--- src/utils/SidebarData.js
+++ src/utils/SidebarData.js
@@ -3,13 +3,13 @@ import { IoMdPersonAdd, IoMdListBox } from "react-icons/io";
 
 export const SidebarData = [
   {
-    title: "Listar",
+    title: "Listar badges",
     path: "/badges",
     icon: <IoMdListBox />,
     className: "Navbar__item fw-light",
   },
   {
-    title: "Agregar",
+    title: "Agregar badge",
     path: "/badges/new",
     icon: <IoMdPersonAdd />,
     className: "Navbar__item fw-light",
